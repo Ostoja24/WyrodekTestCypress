212ce8072336333b2d7aaf621fc935a7827f5528
Ostoja24 <tomasz.ostojski@protonmail.com>
2025-05-05 18:35:32 +0200

    Uncaught exception commit

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -4,6 +4,9 @@ export class BasePage {
         cy.document().should('have.property', 'readyState', 'complete');
         cy.waitForNetworkIdle(500);
         cy.wait(500);
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+          })
         return this;
     }
     protected typeIntoField(fieldSelector: string, text: string): BasePage {
