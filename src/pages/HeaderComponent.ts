381afd99b087dc11849f9446dcbf06520b196b42
Ostoja24 <tomasz.ostojski@protonmail.com>
2025-05-08 19:24:10 +0200

    Commit with fix of Header Component

--- src/pages/HeaderComponent.ts
+++ src/pages/HeaderComponent.ts
@@ -13,7 +13,7 @@ export class HeaderComponent extends BasePage{
     public typeIntoSearchField(text: string): BlogPage {
         cy.wait(1000);
         cy.get(this.searchSelector).should('be.visible');
-        cy.get(this.searchSelector).realClick();
+        cy.get(this.searchSelector).click({force:true});
         cy.get(this.searchField).should('be.visible').should('have.css', 'visibility', 'visible').type(text).type('{enter}');
         return new BlogPage();
     }
